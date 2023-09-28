3c3e3d2fc6199f3ca31dca42c987eff2ed92afb7
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-09-29 01:57:03 +0530

    First commit

---
+++ src/app/Left.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from "./pages/styles/Left.module.css"
+
+const Left = () => {
+  return (
+    <div className={styled.main}>
+        hi
+    </div>
+  )
+}
+
+export default Left
