c6b349b9cedbf1b19b6c2e2dce85d9ff2459b43c
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-10-07 19:47:23 +0530

    Project section

--- src/app/Projects.jsx
+++ src/app/Projects.jsx
@@ -1,13 +1,90 @@
+"use client";
 import React from "react";
-import styles from "./pages/styles/Projects.module.css"
+import styles from "./pages/styles/Projects.module.css";
+import github from "../../public/github1.png";
+import link from "../../public/link.png";
+import portfolio from "../../public/portfolio.jpeg";
+import Image from "next/image";
+import next from "../../public/next.png";
+import react from "../../public/react.png";
+import node from "../../public/node.png";
+import exp from "../../public/expressjs.png";
+import firebase from "../../public/firebase.png";
+import mongo from "../../public/mongo.png";
 const Projects = () => {
   return (
     <>
       <section className={styles.main}>
-      <h1 className={styles.header}>Projects</h1>
+        <h1 className={styles.header}>Projects</h1>
+        <div className={styles.card_container}>
+          <ProjectCard />
+        </div>
       </section>
     </>
   );
 };
 
 export default Projects;
+const TechStack = [next, react, node, exp, mongo, firebase];
+// const TechStack
+const ProjectCard = () => {
+  return (
+    <>
+      <div className={styles.card}>
+        <div
+          style={{
+            width: "140px",
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+            height: "60px",
+            background: "#ffffff",
+            marginLeft: "-25px",
+            padding: "5px 10px 5px 20px",
+            borderRadius: "5px 10px 10px 5px",
+          }}
+        >
+          <span>
+            <Image src={github} style={{ width: "40px", height: "40px" }} />
+          </span>
+          <span>
+            <Image src={link} style={{ width: "40px", height: "40px" }} />
+          </span>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexWrap: "wrap",
+          }}
+        >
+          <div>
+            <h3 className={styles.portfolio_header}>Portfolio 2023</h3>
+            <span style={{width:"350px",display:"flex",margin:"10px 0px"}}>
+              This is my portfolio that showcases my ideology,accomplishments
+              and my achievements.
+            </span>
+            <div className={styles.techstack}>
+              {TechStack.map((item, id) => {
+                return (
+                  <Image
+                    src={item}
+                    style={{
+                      width: "50px",
+                      height: "100%",
+                      boxSizing: "border-box",
+                      padding: "10px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <Image src={portfolio} style={{ width: "50%", height: "100%",borderRadius:"10px"}} />
+        </div>
+      </div>
+    </>
+  );
+};
---
+++ src/app/pages/Static/Project.js
@@ -0,0 +1,23 @@
+import next from "../../public/next.png";
+import react from "../../public/react.png";
+import node from "../../public/node.png";
+import exp from "../../public/expressjs.png";
+import firebase from "../../public/firebase.png";
+import mongo from "../../public/mongo.png";
+import portfolio from "../../public/portfolio.jpeg";
+const Project = [
+  {
+    name: "Portfolio 2023",
+    desc: "This is my portfolio that showcases my ideology,accomplishments and my achievements.",
+    tech:[next,react],
+    img:portfolio
+  },
+  {
+    name: "Daakticket",
+    desc: "A social media webapp in which me and my team tried to incorporate the best features of different social media. I've mostly worked on the frontend and incorporated backend apis with the ",
+    tech:[react,node,exp],
+    img:portfolio
+  },
+  {},
+];
+export default Project;
