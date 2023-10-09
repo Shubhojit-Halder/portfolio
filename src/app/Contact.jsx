a93344711311a66627309367133bdc592ed92756
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-10-09 20:44:26 +0530

    Projects section changes

--- src/app/Contact.jsx
+++ src/app/Contact.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./pages/styles/Contact.module.css";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <>
+      <section className={styles.main}>
+        <h1 className={styles.header}>Contact</h1>
+        <div>
+          <div className={styles.left}>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          </div>
+          <div className={styles.right}></div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Contact
+export default Contact;
