75d8f09a4c5c60ae1ac9172237b6d10b82f9ce31
Shubhojit Halder <shubhojit_h.ece2020@msit.edu.in>
2023-12-04 21:30:19 +0530

    pixels changed to rem

--- src/app/TopName.jsx
+++ src/app/TopName.jsx
@@ -1,16 +1,29 @@
+"use client";
 import React from "react";
 import styles from "./pages/styles/Topname.module.css";
+import { motion } from "framer-motion";
+import { Button } from "@mui/material";
 const TopName = () => {
   return (
     <>
       <div>
-        <div className={styles.head1}>Hey,There!!</div>
+        <motion.div
+          // initial={{x:-100}}
+          animate={{x: 100}}
+          transition={{ ease: "easeOut", duration: 2 }}
+          className={styles.head1}
+          // transition={{ duration: 0.5, delay: 0.25 }}
+        >
+          Hey,There!!
+        </motion.div>
         <div className={styles.head2}>
           I AM
           <br />
           SHUBHOJIT
           <br /> HALDER
         </div>
+        <div className={styles.head1}>A Fullstack Developer.</div>
+        {/* <Button variant="contained" style={{background:"#560bad",}} >View Resume</Button> */}
       </div>
     </>
   );
